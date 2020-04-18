730f58a283aae45c533d8704363da381b873d28e
shiyutong123 <62173463+shiyutong123@users.noreply.github.com>
2020-04-18 17:31:00 +0800

    Update test.js
    update test

--- test.js
+++ test.js
@@ -1,26 +1,25 @@
-const https = require('https'); //导入模块
-const tls = require('tls');
-const fs = require('fs');
-const change_cert = require('./change_cert.js');
+const https = require('https')
+const fs = require('fs')
+const changecert = require('./index.js')
 
-const hostname = '127.0.0.1'; //主机名
-const port = 3000; //端口
+const hostname = '127.0.0.1'
+const port = 3000
 
-const options1 = {
-    key: fs.readFileSync('C:/Users/Administrator/ryans1-key.pem'), //钥匙
-    cert: fs.readFileSync('C:/Users/Administrator/ryans1-cert.pem') //证书
-  };
-const key='C:/Users/Administrator/ryans2-key.pem';//钥匙
-const cert='C:/Users/Administrator/ryans2-cert.pem';//证书
-//创建一个HTTP服务器
-const server = https.createServer(options1 , (req,res) => {
-    res.statusCode = 200;  //状态码
-    res.setHeader('Content-Type','text/plain');  //设置响应头
-    res.end('Hello World'); //快速结束没有任何数据的响应
+const keypath = 'C:/Users/Administrator/ryans1-key.pem'
+const certpath = 'C:/Users/Administrator/ryans1-cert.pem'
+const options = {
+  key: fs.readFileSync(keypath),
+  cert: fs.readFileSync(certpath)
+}
 
-});
-server.listen(port,hostname,()=>{
-    console.log(`Server running at https://${hostname}:${port}/`); //输出log
+const server = https.createServer(options, (req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World')
+})
 
-});
-change_cert(server,key,cert);
+server.listen(port, hostname, () => {
+  console.log(`Server running at https://${hostname}:${port}/`)
+})
+
+changecert(server, keypath, certpath)
