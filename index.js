dfccc9b783607e3cd9f1b6e33db3620765acad16
George Cheng <Gerhut@GMail.com>
2020-04-22 16:34:01 +0800

    Finish the live secure context

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const fs = require('fs')
 
-function changecert (server, keypath, certpath) {
-  var option = {}
-  const result1 = 'key'
-  const result2 = 'cert'
-  watchfile(keypath, option, result1, certpath, server)
-  watchfile(certpath, option, result2, keypath, server)
-}
-
-function watchfile (filepath, option, result, otherpath, server) {
-  fs.watch(filepath, (eventType, filename) => {
-    if (eventType === 'change') {
-      if (result === 'key') {
-        option = {
-          key: fs.readFileSync(filepath),
-          cert: fs.readFileSync(otherpath)
-        }
-      } else if (result === 'cert') {
-        option = {
-          key: fs.readFileSync(otherpath),
-          cert: fs.readFileSync(filepath)
-        }
-      }
+function liveSecureContext (server, options) {
+  const { key, cert, ...secureContext } = options
+  const watchers = [
+    fs.watch(key, () => {
       try {
-        server.setSecureContext(option)
-      } catch {
+        secureContext.key = fs.readFileSync(key)
+        server.setSecureContext(secureContext)
+      } catch (error) {
+        console.error(error)
       }
+    }),
+    fs.watch(cert, () => {
+      try {
+        secureContext.cert = fs.readFileSync(cert)
+        server.setSecureContext(secureContext)
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  ]
+
+  try {
+    secureContext.key = fs.readFileSync(key)
+    secureContext.cert = fs.readFileSync(cert)
+    server.setSecureContext(secureContext)
+  } catch (error) {
+    console.error(error)
+  }
+
+  server.on('close', () => {
+    for (const watcher of watchers) {
+      watcher.close()
     }
   })
+
+  return server
 }
-module.exports = changecert
+
+module.exports = liveSecureContext
